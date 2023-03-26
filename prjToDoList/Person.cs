ac377013a0b9c7743721ad8006664513ff69d8b2
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-26 17:44:54 -0300

    criada a casse Person junto de suas propriedades e metodos

---
+++ prjToDoList/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjToDoList
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public Person SetName (string name)
+        {
+            this.Name = name;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
